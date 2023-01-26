12d456595b32d0b262246b74c0a61aa870a2087d
Sahejdeep Singh <bboysahej@gmail.com>
2023-01-26 02:38:58 -0800

    using react-webcam so it works on mobile devices

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,37 +1,33 @@
 import React, { useRef, useEffect, useState } from 'react';
 import "./MainContent.css"
 import Tesseract from 'tesseract.js';
-
+import Webcam from "react-webcam";
 
 function MainContent() {
     // Create a ref to access the canvas element
-    const canvasRef = useRef(null);
+    /*const canvasRef = useRef(null);
 
     const [recognizedText, setRecognizedText] = useState('');
 
     // Create a state variable to store the data URL of the captured image
-  const [capturedImage, setCapturedImage] = useState(null);
-  
+    const [capturedImage, setCapturedImage] = useState(null);
+    const camera = useCamera({
+        video: true,
+        audio: false
+    });
+    
     useEffect(() => {
-      // Access the camera and stream the video feed to the canvas
-      navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-        const video = document.createElement('video');
-        video.srcObject = stream;
-        video.addEventListener('loadedmetadata', () => {
-          video.play();
-          const context = canvasRef.current.getContext('2d');
-          const draw = () => {
+        if (!camera) return;
+        const video = camera.stream.current;
+        const context = canvasRef.current.getContext('2d');
+        const draw = () => {
             context.drawImage(video, 0, 0, canvasRef.current.width, canvasRef.current.height);
             requestAnimationFrame(draw);
-          };
-          requestAnimationFrame(draw);
-        });
-      });
-    }, []);
+        };
+        requestAnimationFrame(draw);
+    }, [camera]);
 
-        
-
-  function captureImage() {
+    function captureImage() {
     // Get the data URL of the current frame from the canvas
     const dataURL = canvasRef.current.toDataURL();
 
@@ -49,17 +45,45 @@ function MainContent() {
       setRecognizedText(text);
       })
 
-  }
+  }*/
 
-    
+  const webcamRef = useRef(null);
+    const [capturedImage, setCapturedImage] = useState(null);
+    const [recognizedText, setRecognizedText] = useState('');
 
-  
+    function captureImage() {
+        const imageSrc = webcamRef.current.getScreenshot();
+        setCapturedImage(imageSrc);
+        Tesseract.recognize(imageSrc, 'eng', { logger: m => console.log(m) }).then(({ data: { text } }) => {
+            setRecognizedText(text);
+        });
+    }
+
     return (
       <div>
-        <div className='container'>
-          <canvas ref={canvasRef} width={640} height={360} />
-        </div>
-        
+          <Webcam ref={webcamRef} audio={false} width={640} height={480} />
+<button onClick={captureImage}>Capture</button>
+{capturedImage && <img src={capturedImage} alt="Captured image" />}
+{recognizedText ? (
+<div>Recognized text: {recognizedText}</div>
+) : (
+<div>Recognized text: </div>
+)}
+</div>
+);
+}
+
+export default MainContent;
+    
+    
+    
+    /*return (
+      <div>
+        {camera ? (
+        <canvas ref={canvasRef} width={640} height={480} />
+        ) : (
+        <div>Please give permission to access camera</div>
+        )}
         <button onClick={captureImage}>Capture</button>
         {capturedImage && <img src={capturedImage} alt="Captured image" />}
         {recognizedText ? (
@@ -72,4 +96,4 @@ function MainContent() {
     );
   }
 
-export default MainContent
+export default MainContent*/
