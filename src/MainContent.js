bdf1055b9a7370d8d23cd2202d233ec2bc643bfc
Sahejdeep Singh <bboysahej@gmail.com>
2023-01-26 16:09:00 -0800

    updates

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,103 +1,68 @@
 import React, { useRef, useEffect, useState } from 'react';
 import "./MainContent.css"
 import Tesseract from 'tesseract.js';
-import Webcam from "react-webcam";
 
 function MainContent() {
-    // Create a ref to access the canvas element
-    /*const canvasRef = useRef(null);
-
     const [recognizedText, setRecognizedText] = useState('');
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [loading, setLoading] = useState(false);
 
-    // Create a state variable to store the data URL of the captured image
-    const [capturedImage, setCapturedImage] = useState(null);
-    const camera = useCamera({
-        video: true,
-        audio: false
-    });
-    
-    useEffect(() => {
-        if (!camera) return;
-        const video = camera.stream.current;
-        const context = canvasRef.current.getContext('2d');
-        const draw = () => {
-            context.drawImage(video, 0, 0, canvasRef.current.width, canvasRef.current.height);
-            requestAnimationFrame(draw);
-        };
-        requestAnimationFrame(draw);
-    }, [camera]);
-
-    function captureImage() {
-    // Get the data URL of the current frame from the canvas
-    const dataURL = canvasRef.current.toDataURL();
-
-    // Set the data URL as the value of the capturedImage state variable
-    setCapturedImage(dataURL);
-
-    console.log(dataURL);
-
-    setTimeout(5000);
-
-    Tesseract.recognize(
-      dataURL,
-      'eng',
-      { logger: m => console.log (m) } ). then (({ data: { text } }) => {
-      setRecognizedText(text);
-      })
-
-  }*/
-
-  const videoConstraints = {
-    facingMode: { exact: "environment" }
-  };
-
-  const webcamRef = useRef(null);
-    const [capturedImage, setCapturedImage] = useState(null);
-    const [recognizedText, setRecognizedText] = useState('');
-
-    function captureImage() {
-        const imageSrc = webcamRef.current.getScreenshot();
-        setCapturedImage(imageSrc);
-        Tesseract.recognize(imageSrc, 'eng', { logger: m => console.log(m) }).then(({ data: { text } }) => {
+    function recognizeText() {
+      setLoading(true);
+        Tesseract.recognize(URL.createObjectURL(selectedImage), 'eng', { logger: m => console.log(m) }).then(({ data: { text } }) => {
             setRecognizedText(text);
         });
+      setLoading(false);
     }
+
+    
 
     return (
       <div>
-          <Webcam ref={webcamRef} audio={false} width={640} height={480} videoConstraints={videoConstraints} />
-          <button onClick={captureImage}>Capture</button>
-          {capturedImage && <img src={capturedImage} alt="Captured image" />}
-          {recognizedText ? (
-            <div>Recognized text: {recognizedText}</div>
-          ) : (
-            <div>Recognized text: </div>
-          )}
+        <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Text Recognizer</span></h1>
+
+        {selectedImage ? 
+          <>
+            <img class="h-auto max-w-full" src={URL.createObjectURL(selectedImage)} alt="image description"></img>
+            
+          </> :
+          <div class="flex items-center justify-center w-full">
+          <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+              <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                  <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Capture or upload a document image</span></p>
+                  <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+              </div>
+              <input id="dropzone-file" type="file" onChange={(e)=>setSelectedImage(e.target.files[0])} class="hidden" />
+          </label>
+        </div> 
+        }
+        {selectedImage && 
+          <>
+              {loading ? 
+              <>
+                <button disabled type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center">
+                    <svg aria-hidden="true" role="status" class="inline w-4 h-4 mr-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+                    </svg>
+                    Loading...
+                </button>
+              </>  
+              : <button onClick={recognizeText} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Recognize Text</button>}
+              <label class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium cursor-pointer rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                Upload or Capture New Image
+              <input id="image-file" type="file" onChange={(e)=>setSelectedImage(e.target.files[0])} class="hidden" />
+            </label>
+          </>}
+        <h5 class="text-2xl mt-8 font-bold text-black">Recognized Text:</h5>
+        {selectedImage ? (
+          recognizedText? <p>{recognizedText}</p> : <p>No text recognized yet</p>) : 
+          <p>Upload or capture an image to recognize text</p>
+          }
+        
       </div>
 );
 }
 
 export default MainContent;
-    
-    
-    
-    /*return (
-      <div>
-        {camera ? (
-        <canvas ref={canvasRef} width={640} height={480} />
-        ) : (
-        <div>Please give permission to access camera</div>
-        )}
-        <button onClick={captureImage}>Capture</button>
-        {capturedImage && <img src={capturedImage} alt="Captured image" />}
-        {recognizedText ? (
-        <div>Recognized text: {recognizedText}</div>
-      ) : (
-        <div>Recognized text: </div>
-      )}
-
-      </div>
-    );
-  }
-
-export default MainContent*/
